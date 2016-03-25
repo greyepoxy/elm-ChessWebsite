58b1242265d2f6c988158f07e0cb42324027b92f
Justin Huffaker <greyepoxy2@gmail.com>
2016-03-25 00:17:10 -0700

    added first set of move checks for pawn in its initial position.

--- src/App/Chessboard/Model.elm
+++ src/App/Chessboard/Model.elm
@@ -1,4 +1,8 @@
-module App.Chessboard.Model (Team(White,Black), ChessPiece(King,Queen,Rook,Bishop,Knight,Pawn), BoardSquare(Empty,FilledWith), Row, Chessboard, initialBoard) where
+module App.Chessboard.Model (
+  Team(White,Black)
+  , ChessPiece(King,Queen,Rook,Bishop,Knight,Pawn)
+  , BoardSquare(Empty,FilledWith)
+  , Row, Chessboard, initialBoard, getPossibleMoveLocations) where
 
 import Array exposing (Array)
 
@@ -53,3 +57,58 @@ getInitialTopOrBottomRow team = Array.fromList [
     , FilledWith team Rook
   ]
 
+type alias BoardSquareWithLoc = {
+    boardPosition: (Int,Int)
+    , square: BoardSquare
+  }
+
+{--
+  Get every possible move location for a given board square
+  at the given location in the given chessboard. 
+--}
+getPossibleMoveLocations: Chessboard -> (Int,Int) -> List (Int, Int)
+getPossibleMoveLocations board testLoc =
+  let
+    maybeSquare = getBoardSquareAtLocation board testLoc
+  in
+    case maybeSquare of
+      Nothing -> []
+      Just square -> case square of
+        Empty -> []
+        FilledWith team piece ->
+          case piece of
+            Pawn -> 
+              getIdealPawnMoveLocations team testLoc 
+                |> List.filter (canPieceMoveToLoc board)
+                |> List.map (\moveLoc -> moveLoc.loc)
+            _ -> []
+
+getBoardSquareAtLocation: Chessboard -> (Int,Int) -> Maybe BoardSquare
+getBoardSquareAtLocation board (x,y) =
+  Array.get y board.squares
+    `Maybe.andThen` Array.get x
+
+type CanMoveIf = 
+  LocationEmpty
+  | LocationContainsOpponent
+
+type alias MoveLoc = {
+    conditions: CanMoveIf
+    , loc: (Int, Int)
+  }
+ 
+canPieceMoveToLoc: Chessboard -> MoveLoc -> Bool
+canPieceMoveToLoc board moveLoc =
+  True
+
+getIdealPawnMoveLocations: Team -> (Int,Int) -> List MoveLoc
+getIdealPawnMoveLocations team (curX,curY) =
+  let
+    moveDir = if team == White then 1 else -1
+    atStartLoc = case team of
+      White -> if curY == 1 then True else False
+      Black -> if curY == 6 then True else False
+    additionalMoveLoc = if atStartLoc then [{conditions = LocationEmpty, loc = (curX,curY+2*moveDir)}] else [] 
+  in
+    [{conditions = LocationEmpty, loc = (curX,curY+moveDir)}]
+      |> List.append additionalMoveLoc
--- src/App/Chessboard/Tests.elm
+++ src/App/Chessboard/Tests.elm
@@ -26,13 +26,13 @@ viewTests =
 testsForGetSquareColor =
   suite "Should be able to getSquareColor"
     [ test "(0,0) -> white" <|
-        getSquareColor (0,0) `assertEqual` Color.white
+        Color.white `assertEqual` getSquareColor (0,0)
       , test "(1,0) -> charcoal" <|
-        getSquareColor (1,0) `assertEqual` Color.charcoal
+        Color.charcoal `assertEqual` getSquareColor (1,0)
       , test "(0,1) -> charcoal" <|
-        getSquareColor (0,1) `assertEqual` Color.charcoal
+        Color.charcoal `assertEqual` getSquareColor (0,1)
       , test "(1,3) -> white" <|
-        getSquareColor (1,3) `assertEqual` Color.white
+        Color.white `assertEqual` getSquareColor (1,3)
     ]
 
 testDrawableSquare: DrawableSquare
@@ -45,33 +45,49 @@ testDrawableSquare = {
 testsForViewMarkDrawableSquareAsSelected =
   suite "Can mark Drawable Squares As Selected"
     [ test "Nothing should return original square" <|
-        (markDrawableSquareAsSelected Nothing testDrawableSquare) `assertEqual` testDrawableSquare
+        testDrawableSquare `assertEqual` (markDrawableSquareAsSelected Nothing testDrawableSquare)
       , test "Different board position should return original square" <|
-        (markDrawableSquareAsSelected (Just (1,1)) testDrawableSquare) `assertEqual` testDrawableSquare
+        testDrawableSquare `assertEqual` (markDrawableSquareAsSelected (Just (1,1)) testDrawableSquare)
       , test "Same board position should return original square selected" <|
-        (markDrawableSquareAsSelected (Just (0,0)) testDrawableSquare) 
+        { testDrawableSquare | selected = True } 
         `assertEqual` 
-        { testDrawableSquare | selected = True }
+        (markDrawableSquareAsSelected (Just (0,0)) testDrawableSquare)
       , test "Same board position but Empty square should return original square" <|
+        { testDrawableSquare | square = Empty} 
+        `assertEqual`
         (markDrawableSquareAsSelected (Just (0,0)) { testDrawableSquare | square = Empty}) 
-        `assertEqual` 
-        { testDrawableSquare | square = Empty}
     ]
  
 canConvertFromPosAndSquareToDrawableSquare =
   test "Can convert from position and square to DrawableSquare" <|
-    convertSquareToDrawableSquare (0,0) (FilledWith White Pawn)
-    `assertEqual`
     testDrawableSquare
+    `assertEqual`
+    convertSquareToDrawableSquare (0,0) (FilledWith White Pawn)
 
 canConvertFromBoardPositionToScreenCoordinates =
-  test "Can convert from board position and square to screen coordinates" <|
-  translateToScreenCoordinates 100 (2,5) `assertEqual` (200, 500)
+  test "Can convert from board position and square to screen coordinates"
+    <| (200, 500) `assertEqual` translateToScreenCoordinates 100 (2,5)
 
 
 modelTests : Test
 modelTests =
   suite "Model Tests"
-    [ 
+    [ testMoveLocationsForEmptySquare
+      , testMoveLocationsForPawnFilledSquare
     ]
 
+testMoveLocationsForEmptySquare =
+  test "Should be no possible move locations for an Empty Square"
+    <| [] `assertEqual` getPossibleMoveLocations initialBoard (2,5)
+
+testMoveLocationsForPawnFilledSquare =
+  suite "Pawn should move correctly"
+  [ test "Should be able to move one space or two on first move"
+      <| assert (containsAll [(0,2), (0,3)] (getPossibleMoveLocations initialBoard (0,1)))
+  ]
+
+containsAll: List a -> List a -> Bool
+containsAll itemsToCheckFor list =
+  itemsToCheckFor
+    |> List.map (\a -> List.member a list)
+    |> List.foldl (\a b -> a && b) True
